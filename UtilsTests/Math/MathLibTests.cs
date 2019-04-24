fff0aeaf5777d0ca14558f59782969b2ab93219d
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-04-24 11:44:12 +0000

    Initial commit
    git-svn-id: svn://127.0.0.1/trunk@2 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsTests/Math/MathLibTests.cs
@@ -0,0 +1,27 @@
+﻿using UtilsLib.Math;
+using Xunit;
+
+namespace UtilsTests.Math
+{
+	public class MathLibTests
+	{
+		[Theory]
+		[InlineData(1, 2, 1)]
+		[InlineData(1, 1, 0)]
+		[InlineData(0, 1, 0)]
+		[InlineData(2, 10, 2)]
+		[InlineData(12, 10, 2)]
+		[InlineData(22, 10, 2)]
+		[InlineData(-2, 10, 8)]
+		[InlineData(-12, 10, 8)]
+		[InlineData(-22, 10, 8)]
+		[InlineData(-8, 12, 4)]
+		[InlineData(8, 12, 8)]
+		[InlineData(37, 12, 1)]
+		public void Mod(int input, int m, int expected)
+		{
+			var result = MathLib.Mod(input, m);
+			Assert.Equal(expected, result);
+		}
+	}
+}
