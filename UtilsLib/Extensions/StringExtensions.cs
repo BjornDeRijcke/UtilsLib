c3b3af7d3d6e275aac7f3102f33424045ff1caf5
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-05-31 12:35:13 +0000

    Added Extensions/StringExtensions.cs with corresponding tests
    git-svn-id: svn://127.0.0.1/trunk@6 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsLib/Extensions/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace UtilsLib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveControlChars(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return "";
+
+            return new string(s.Where(c => !Char.IsControl(c)).ToArray());
+        }
+
+        /// <summary>
+        /// Creates a new string where all unicode cateogry 15 'Format' (Cf) characters are removed.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>string withouth format characters</returns>
+        public static string RemoveFormatChars(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return "";
+
+            return new string(s.Where(c => !c.IsUnicodeFormatChar()).ToArray());
+        }
+
+        public static string RemoveControlAndFormatChars(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return "";
+
+            return new string(s.Where(c => !c.IsUnicodeFormatChar() && !Char.IsControl(c)).ToArray());
+        }
+
+        /// <summary>
+        /// Returns the input string with the first character converted to uppercase, 
+        /// or mutates any nulls passed into string.Empty
+        /// </summary>
+        public static string FirstCharacterToUpperCase(this string s)
+        {
+            if (String.IsNullOrEmpty(s))
+                return String.Empty;
+
+            char[] a = s.ToLowerInvariant().ToCharArray();
+            a[0] = Char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
---
+++ UtilsTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using UtilsLib.Extensions;
+using Xunit;
+
+namespace UtilsTests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("ᠣᠩᠭᠣᠯ \a ᠢᠴᠢᠭ", "ᠣᠩᠭᠣᠯ  ᠢᠴᠢᠭ")]
+        public void RemoveControlChars(string input, string expected)
+        {
+            var res = input.RemoveControlChars();
+            Assert.Equal(expected, res);
+        }
+
+        [Theory]
+        [InlineData("ᠣᠩᠭᠣᠯ \u180E ᠢᠴᠢᠭ", "ᠣᠩᠭᠣᠯ  ᠢᠴᠢᠭ")]
+        public void RemoveFormatChars(string input, string expected)
+        {
+            var res = input.RemoveFormatChars();
+            Assert.Equal(expected, res);
+        }
+
+        [Theory]
+        [InlineData("ᠣᠩᠭᠣᠯ \a ᠢᠴᠢᠭ\u180E", "ᠣᠩᠭᠣᠯ  ᠢᠴᠢᠭ")]
+        public void RemoveControlAndFormatChars(string input, string expected)
+        {
+            var res = input.RemoveControlAndFormatChars();
+            Assert.Equal(expected, res);
+        }
+
+        [Theory]
+        [InlineData("1lem", "1lem")]
+        [InlineData("Alem", "Alem")]
+        [InlineData("_LK", "_lk")]
+        [InlineData("a first song", "A first song")]
+        [InlineData("\t", "\t")]
+        [InlineData("DeKeNEnalMAls", "Dekenenalmals")]
+        [InlineData("deKeNEnalMAls", "Dekenenalmals")]
+        public void FirstCharacterToUpperCase(string input, string expected)
+        {
+            var res = input.FirstCharacterToUpperCase();
+            Assert.Equal(expected, res);
+        }
+    }
+}
