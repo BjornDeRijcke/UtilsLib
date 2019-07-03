595f45c2b95f17d98186448e5dbe6c48c7674847
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-07-03 16:02:01 +0000

    Added Extensions/IEnumerableExtensions.cs with corresponding tests
    git-svn-id: svn://127.0.0.1/trunk@7 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsLib/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtilsLib.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<TSource> Distinct<TSource, TProperty>(this IEnumerable<TSource> source, Func<TSource, TProperty> equalityProperty)
+        {
+            if (source != null)
+            {
+                HashSet<TProperty> seenKeys = new HashSet<TProperty>();
+                foreach (var elem in source)
+                {
+                    if (seenKeys.Add(equalityProperty(elem)))
+                    {
+                        yield return elem;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UtilsTests/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UtilsLib.Extensions;
+using Xunit;
+
+namespace UtilsTests.Extensions
+{
+    public class IEnumerableExtensionsTests
+    {
+        [Fact]
+        public void Distinct()
+        {
+            var input = new DistinctTestClass[] { new DistinctTestClass { Prop = 'a' }, new DistinctTestClass { Prop = 'a' }, new DistinctTestClass { Prop = '\u00AD' }, new DistinctTestClass { Prop = 'µ' }, new DistinctTestClass { Prop = 'µ' }, new DistinctTestClass { Prop = '±' } }.AsEnumerable();
+            var expected = new DistinctTestClass[] { new DistinctTestClass { Prop = 'a' }, new DistinctTestClass { Prop = '\u00AD' }, new DistinctTestClass { Prop = 'µ' }, new DistinctTestClass { Prop = '±' } };
+
+            var output = input.Distinct(i => i.Prop).ToArray();
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Assert.Equal(expected[i].Prop, output[i].Prop);
+            }
+        }
+
+        private class DistinctTestClass
+        {
+            public char Prop { get; set; }
+        }
+    }
+}
