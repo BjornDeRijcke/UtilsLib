6ffb8b4963982e922d1403a87bc1af85b681e3cb
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-07-09 08:39:51 +0000

    Added Extensions/ListExtensions.cs with corresponding tests
    git-svn-id: svn://127.0.0.1/trunk@8 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsLib/Extensions/ListExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtilsLib.Extensions
+{
+    public static class ListExtensions
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static IEnumerable<List<T>> Split<T>(this List<T> list, int size)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+
+            if (list.Count <= size)
+            {
+                yield return new List<T>(list);
+            }
+            else
+            {
+                for (int i = 0; i < list.Count; i += size)
+                {
+                    yield return list.GetRange(i, System.Math.Min(size, list.Count - i));
+                }
+            }
+        }
+
+        public static void ExtractFirst<T>(this List<T> list, out T extractedItem)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+
+            if (list.Count < 1)
+            {
+                extractedItem = default(T);
+            }
+            else
+            {
+                extractedItem = list[0];
+                list.Remove(extractedItem);
+            }
+        }
+    }
+}
---
+++ UtilsTests/Extensions/ListExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UtilsLib.Extensions;
+using Xunit;
+
+namespace UtilsTests.Extensions
+{
+    public class ListExtensionsTests
+    {
+        [Fact]
+        public void Shuffle_WhenShuffling_OrderHasChanged()
+        {
+            var lst = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var exp = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            lst.Shuffle();
+
+            Assert.NotEqual(exp, lst);
+        }
+
+        [Fact]
+        public void Split_WhenSplittingWithExactDividableCountAndSize_GeneratesSequencesOfExactlyRequestedSize()
+        {
+            var lst = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var size = 2;
+
+            foreach (var section in lst.Split(size))
+                Assert.Equal(size, section.Count);
+        }
+
+        [Fact]
+        public void Split_WhenSplittingWithNonExactDividableCountAndSize_LastGeneratedSequenceIsSmallerThenSize()
+        {
+            var lst = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var size = 6;
+
+            var splitLists = new List<List<int>>(lst.Split(size));
+
+            Assert.Equal(2, splitLists.Count);
+            Assert.Equal(size, splitLists[0].Count);
+            Assert.Equal(3, splitLists[1].Count);
+        }
+
+        [Fact]
+        public void ExtractFirst_WhenExtracting_ElementHasReallyBeenRemoved()
+        {
+            var lst = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            lst.ExtractFirst(out int extractedItem);
+
+            Assert.Equal(0, extractedItem);
+            Assert.DoesNotContain(extractedItem, lst);
+        }
+    }
+}
