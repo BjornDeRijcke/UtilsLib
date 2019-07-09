4451d10c04c0f90c242afb90faa1df9594a33e62
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-07-09 08:52:24 +0000

    Added Extensions/DayOfWeekExtensions.cs with corresponding tests
    git-svn-id: svn://127.0.0.1/trunk@9 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsLib/Extensions/DayOfWeekExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UtilsLib.Extensions
+{
+    public static class DayOfWeekExtensions
+    {
+        public static int DaysUntil(this DayOfWeek dayOfWeek, DayOfWeek otherDayOfWeek)
+        {
+            int day1 = (int)dayOfWeek;
+            int day2 = (int)otherDayOfWeek;
+            return (7 + (day2 - day1)) % 7;
+        }
+    }
+}
---
+++ UtilsTests/Extensions/DayOfWeekExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UtilsLib.Extensions;
+using Xunit;
+
+namespace UtilsTests.Extensions
+{
+    public class DayOfWeekExtensionsTests
+    {
+        [Theory]
+        [InlineData(DayOfWeek.Monday, DayOfWeek.Wednesday, 2)]
+        [InlineData(DayOfWeek.Monday, DayOfWeek.Sunday, 6)]
+        [InlineData(DayOfWeek.Friday, DayOfWeek.Monday, 3)]
+        [InlineData(DayOfWeek.Wednesday, DayOfWeek.Monday, 5)]
+        [InlineData(DayOfWeek.Monday, DayOfWeek.Monday, 0)]
+        [InlineData(DayOfWeek.Tuesday, DayOfWeek.Thursday, 2)]
+        [InlineData(DayOfWeek.Tuesday, DayOfWeek.Saturday, 4)]
+        public void DaysUntil(DayOfWeek day1, DayOfWeek day2, int expected)
+        {
+            var daysUntil = day1.DaysUntil(day2);
+            Assert.Equal(expected, daysUntil);
+        }
+    }
+}
