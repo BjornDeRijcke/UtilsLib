f984892b6f505118832bfde58d93de3171b1b1bc
derijckeb <derijckeb@01d18d36-1c05-6145-b885-60233dafb177>
2019-07-09 09:16:48 +0000

    Added Extensions/DateTimeExtensions.cs with corresponding tests
    git-svn-id: svn://127.0.0.1/trunk@10 01d18d36-1c05-6145-b885-60233dafb177

---
+++ UtilsLib/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace UtilsLib.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        // Note: Week 1 is the 1st week of the year with a Thursday in it.
+        public static int GetIso8601WeekOfYear(this DateTime date)
+        {
+            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+            // be the same week# as whatever Thursday, Friday or Saturday are,
+            // and we always get those right
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(date);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                date = date.AddDays(3);
+            }
+
+            // Return the week of our adjusted day
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
---
+++ UtilsTests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UtilsLib.Extensions;
+using Xunit;
+
+namespace UtilsTests.Extensions
+{
+    public class DateTimeExtensionsTests
+    {
+        [Theory]
+        [ClassData(typeof(GetIso8601WeekOfYearTestData))]
+        public void GetIso8601WeekOfYear(DateTime date, int expectedWeekNumber)
+        {
+            var wn = date.GetIso8601WeekOfYear();
+            Assert.Equal(expectedWeekNumber, wn);
+        }
+
+        public class GetIso8601WeekOfYearTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new DateTime(2033, 6, 6), 23 };
+                yield return new object[] { new DateTime(2024, 12, 30), 1 };
+                yield return new object[] { new DateTime(2024, 12, 31), 1 };
+                yield return new object[] { new DateTime(2023, 2, 27), 9 };
+                yield return new object[] { new DateTime(2018, 1, 1), 1 };
+                yield return new object[] { new DateTime(2017, 1, 1), 52 };
+                yield return new object[] { new DateTime(2052, 10, 19), 42 };
+                yield return new object[] { new DateTime(1976, 1, 1), 1 };
+                yield return new object[] { new DateTime(1976, 12, 31), 53 };
+                yield return new object[] { new DateTime(1977, 1, 2), 53 };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+    }
+}
