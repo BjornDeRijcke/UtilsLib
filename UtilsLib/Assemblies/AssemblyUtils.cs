c9c2affef08ddf2ef4dd06d041fc527a099857c0
Bjorn De Rijcke <bjornderijcke@smartphoto.com>
2019-07-26 07:56:05 +0200

    Added Assemblies/AssemblyUtils.cs with corresponding tests

---
+++ UtilsLib/Assemblies/AssemblyUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace UtilsLib.Assemblies
+{
+    public enum RequiredAssembly
+    {
+        ExecutingAssembly = 0,
+        CallingAssembly = 1,
+        EntryAssembly = 2,
+    }
+
+    public class AssemblyUtils
+    {
+        public static string GetInformationalVersion()
+            => GetInformationalVersion(RequiredAssembly.ExecutingAssembly);
+
+        public static string GetInformationalVersion(RequiredAssembly requiredAssembly)
+        {
+            Assembly assembly;
+
+            switch (requiredAssembly)
+            {
+                case RequiredAssembly.ExecutingAssembly:
+                    assembly = Assembly.GetExecutingAssembly();
+                    break;
+
+                case RequiredAssembly.CallingAssembly:
+                    assembly = Assembly.GetCallingAssembly();
+                    break;
+
+                case RequiredAssembly.EntryAssembly:
+                    assembly = Assembly.GetEntryAssembly();
+                    break;
+
+                default:
+                    throw new ArgumentException("Unknown required assembly", nameof(requiredAssembly));
+            }
+
+            var attr = assembly
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)
+                as AssemblyInformationalVersionAttribute[];
+
+            if (attr.Length > 0)
+                return attr[0]?.InformationalVersion ?? "";
+
+            return "";
+        }
+    }
+}
---
+++ UtilsTests.Assemblies.AssemblyUtils/AssemblyUtilsTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace UtilsTests.Assemblies.AssemblyUtils
+{
+    public class AssemblyUtilsTests
+    {
+        [Fact]
+        public void GetInformationalVersion()
+        {
+            var version = UtilsLib.Assemblies.AssemblyUtils.GetInformationalVersion(UtilsLib.Assemblies.RequiredAssembly.CallingAssembly);
+            Assert.Equal("MYVERSION", version);
+        }
+    }
+}
