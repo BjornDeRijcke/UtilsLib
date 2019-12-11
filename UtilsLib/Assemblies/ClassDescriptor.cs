75652ceda7246cce819144dcc6f8960709293645
Bjorn De Rijcke <adder@live.be>
2019-12-11 11:01:46 +0100

    Added ClassDescriptor and test

---
+++ UtilsLib/Assemblies/ClassDescriptor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using UtilsLib.Extensions;
+
+namespace UtilsLib.Assemblies
+{
+    public class ClassDescriptor
+    {
+        public static ClassDescription ForClass(Type classType)
+        {
+            if (classType == null)
+                throw new ArgumentNullException(nameof(classType));
+
+            if (!classType.IsClass && !classType.IsInterface)
+                throw new ArgumentException("Type should be a class or interface", nameof(classType));
+
+            var attributes = classType.GetCustomAttributes();
+            var dspName = (attributes.FirstOrDefault(att => att.GetType() == typeof(DisplayNameAttribute)) as DisplayNameAttribute)?.DisplayName;
+
+            return new ClassDescription
+            {
+                DisplayName = dspName,
+                ClassType = classType.Name,
+                Properties = GetPublicPropertiesOfType(classType).ToList()
+            };
+        }
+
+        public static List<ClassDescription> DerivativesOfBaseClass(Type baseClass)
+        {
+            var assembly = Assembly.GetAssembly(baseClass);
+            var descriptions = new List<ClassDescription>();
+
+            foreach (var t in assembly.GetTypes())
+            {
+                if (t == null)
+                    continue;
+
+                if (t.BaseType == null)
+                    continue;
+
+                if (t.BaseType == baseClass)
+                    descriptions.Add(ForClass(t));
+            }
+
+            return descriptions;
+        }
+
+        private static IEnumerable<ClassProperty> GetPublicPropertiesOfType(Type t)
+        {
+            foreach (var property in t.GetProperties())
+            {
+                var attributes = property.GetCustomAttributes();
+                var defaultValue = (attributes.FirstOrDefault(att => att.GetType() == typeof(DefaultValueAttribute)) as DefaultValueAttribute)?.Value;
+                var propertyType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+
+                var cp = new ClassProperty
+                {
+                    Name = property.Name?.LowercaseFirst(),
+                    PropertyType = TypeToString(propertyType),
+                    Required = attributes.Any(att => att.GetType() == typeof(RequiredAttribute)),
+                    DefaultValue = defaultValue
+                };
+
+                if (cp.Required && cp.DefaultValue != null)
+                    throw new Exception("Properties with required flag cannot have a default value");
+
+                if (!propertyType.IsValueType && !propertyType.IsPrimitive && propertyType != typeof(String))
+                    cp.Properties = GetPublicPropertiesOfType(property.PropertyType).ToList();
+
+                yield return cp;
+            }
+
+            yield break;
+        }
+
+        private static string TypeToString(Type t)
+        {
+            if (t == typeof(String))
+                return "string";
+
+            if (t == typeof(Byte) || t == typeof(Int16) || t == typeof(Int32) || t == typeof(Int64))
+                return "integer";
+
+            if (t == typeof(Single) || t == typeof(Double))
+                return "number";
+
+            if (t == typeof(Boolean))
+                return "boolean";
+
+            if (t == typeof(DateTime) || t == typeof(DateTimeOffset))
+                return "datetime";
+
+            return "object:" + t.Name;
+        }
+    }
+
+    public class ClassDescription
+    {
+        public string DisplayName { get; set; }
+
+        public string ClassType { get; set; }
+
+        public List<ClassProperty> Properties { get; set; }
+    }
+
+    public class ClassProperty
+    {
+        public string Name { get; set; }
+
+        public string PropertyType { get; set; }
+
+        public List<ClassProperty> Properties { get; set; }
+
+        public bool Required { get; set; }
+
+        public object DefaultValue { get; set; }
+    }
+}
---
+++ UtilsTests/Assemblies/ClassDescriptorTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using UtilsLib.Assemblies;
+using Xunit;
+
+namespace UtilsTests.Assemblies
+{
+    public class ClassDescriptorTests
+    {
+        [Fact]
+        public void WhenCreatingDescription_ValidClass_ReturnsCorrectDescription()
+        {
+            // Arrange
+            var type = typeof(Pain008);
+
+            // Act
+            var description = ClassDescriptor.ForClass(type);
+
+            // Assert
+            Assert.Equal("PAIN 008", description.DisplayName);
+            Assert.Equal("Pain008", description.ClassType);
+            Assert.Equal(4, description.Properties.Count);
+
+            Assert.Equal("myFirstString", description.Properties[0].Name);
+            Assert.Equal("ELKMON", description.Properties[0].DefaultValue);
+            Assert.Equal("string", description.Properties[0].PropertyType);
+            Assert.False(description.Properties[0].Required);
+
+            Assert.Equal("mySecondString", description.Properties[1].Name);
+            Assert.Null(description.Properties[1].DefaultValue);
+            Assert.Equal("string", description.Properties[1].PropertyType);
+            Assert.True(description.Properties[1].Required);
+
+            Assert.Equal("classAA", description.Properties[2].Name);
+            Assert.Null(description.Properties[2].DefaultValue);
+            Assert.Equal("object:ThresholdSettings", description.Properties[2].PropertyType);
+            Assert.True(description.Properties[2].Required);
+
+            Assert.Equal("started", description.Properties[3].Name);
+            Assert.Null(description.Properties[3].DefaultValue);
+            Assert.Equal("datetime", description.Properties[3].PropertyType);
+            Assert.False(description.Properties[3].Required);
+        }
+
+        [DisplayName("PAIN 008")]
+        private class Pain008
+        {
+            [DefaultValue("ELKMON")]
+            public string MyFirstString { get; set; } = "ELKMON";
+
+            [Required]
+            public string MySecondString { get; set; }
+
+            [Required]
+            public ThresholdSettings ClassAA { get; set; }
+
+            public DateTime? Started { get; set; }
+        }
+
+        private class ThresholdSettings
+        {
+            public int UpperBoundX { get; set; }
+
+            public int LowerBoundx { get; set; }
+
+            public int UpperBoundQ { get; set; }
+
+            public int LowerBoundQ { get; set; }
+
+            public float? Divider { get; set; }
+        }
+    }
+}
